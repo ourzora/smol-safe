d910620d9308c6fe9ad99d481e901ddd0eaa5a53
iain nash <me@iain.in>
2023-07-12 14:39:23 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-small front-end for gnosis safe
+small on-chain front-end for gnosis safe
+
+this allows for gnosis safe interactions without requiring any specific backend-servers etc. utilizing safe protocol kit and a web3 injected connector. it's especially useful for new chains that may not be fully integrated into the official gnosis safe UI or as a backup user interface. All signatures in this app are created on-chain.
+
+contributions are welcome. to add a new chain that is not yet in gnosis-deploys, update the chains.ts file.
--- src/app.ts
+++ src/app.ts
@@ -6,35 +6,9 @@ import Safe, {
 import Toastify from "toastify-js";
 import { ethers } from "ethers";
 import { parseEther } from "ethers/lib/utils";
-import '@zoralabs/zorb/dist/component.umd'
+import "@zoralabs/zorb/dist/component.umd";
 import "toastify-js/src/toastify.css";
-
-const defaultL2Addresses = {
-  multiSendAddress: "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761",
-  safeMasterCopyAddress: "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
-  safeProxyFactoryAddress: "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
-  multiSendCallOnlyAddress: "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D",
-  fallbackHandlerAddress: "0x1AC114C2099aFAf5261731655Dc6c306bFcd4Dbd",
-  createCallAddress: "0x7cbB62EaA69F79e6873cD1ecB2392971036cFAa4",
-  signMessageLibAddress: "0xA65387F16B013cf2Af4605Ad8aA5ec25a2cbA3a2",
-};
-
-const baseL2Addresses = {
-  multiSendAddress: "0x998739BFdAAdde7C933B942a68053933098f9EDa",
-  safeMasterCopyAddress: "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
-  safeProxyFactoryAddress: "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
-  multiSendCallOnlyAddress: "0xA1dabEF33b3B82c7814B6D82A79e50F4AC44102B",
-  fallbackHandlerAddress: "0x017062a1dE2FE6b99BE3d9d37841FeD19F573804",
-  createCallAddress: "0xB19D6FFc2182150F8Eb585b79D4ABcd7C5640A9d",
-  signMessageLibAddress: "0x98FFBBF51bb33A056B08ddf711f289936AafF717",
-};
-
-const contractNetworks: ContractNetworksConfig = {
-  [999]: defaultL2Addresses,
-  [7777777]: defaultL2Addresses,
-  [84531]: baseL2Addresses,
-  [8453]: baseL2Addresses,
-};
+import { contractNetworks } from "./chains";
 
 function log(text) {
   console.log(text);
